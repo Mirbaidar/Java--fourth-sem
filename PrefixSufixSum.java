48ae34c4637ca2041b8b0d2f370c9c6d7dad5d4f
BAIDAR BAKHAT <mirbaidarmirbaidar@gmail.com>
2024-05-24 21:40:47 +0530

    Equal partition
    partition on the  basis sum prefix sum and total sum

--- PrefixSufixSum.java
+++ PrefixSufixSum.java
@@ -6,7 +6,22 @@ public class PrefixSufixSum {
         for (int Ele : arr) {
             sum = sum + Ele;
         }
-        return 1;
+        int i = 1;
+        while (i < arr.length ) {
+            arr[i] = arr[i - 1] + arr[i];
+            sum = sum - arr[i];
+
+            if (arr[i] == sum) {
+
+                System.out.println("yes done");
+
+                return 1;
+
+            }
+            i++;
+
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -20,7 +35,12 @@ public class PrefixSufixSum {
             arr[i] = inp.nextInt();
         }
 
-        partition(arr);
+        int get = partition(arr);
+        if (get == 1) {
+            System.out.println("Partition complete : ");
+        } else {
+            System.out.println("The array can not be divided into two parts ");
+        }
 
         inp.close();
     }
